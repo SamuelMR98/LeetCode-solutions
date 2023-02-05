62e6fd6ec0724fadad3032ccc1a97d870eda201c
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2023-02-04 21:00:53 -0700

    reverse-integer solution

---
+++ reverse-integer.py
@@ -0,0 +1,14 @@
+input = int(input())
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            x = int(str(x)[::-1][:-1]) * -1
+        else:
+            x = int(str(x)[::-1])
+        if x > 2**31 - 1 or x < -2**31:
+            return 0
+        return x
+
+if __name__ == "__main__":
+    print(Solution().reverse(input))
