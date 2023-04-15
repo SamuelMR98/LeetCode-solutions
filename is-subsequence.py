8ecfcf2900043bdfb92693ca5ad9c3878effb719
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2023-04-14 22:00:39 -0600

    is-subsequence solution

---
+++ is-subsequence.py
@@ -0,0 +1,17 @@
+s = "abc"
+t = "ahbgdc"
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False
+        if s[0] == t[0]:
+            return self.isSubsequence(s[1:], t[1:])
+        else:
+            return self.isSubsequence(s, t[1:])
+    
+
+if __name__ == "__main__":
+    print(Solution().isSubsequence(s, t))
--- isomorphic-strings.py
+++ isomorphic-strings.py
@@ -1,9 +1,16 @@
-s = 'egg'
-t = 'add'
+s = 'foo'
+t = 'bar'
 
 class Solution:
-    def isIsomorphic(sel, s: str, t: str) -> bool:
-        return
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        if len(s) == 0:
+            return True
+        if s[0] == t[0]:
+            return self.isIsomorphic(s[1:], t[1:])
+        else:
+            return self.isIsomorphic(s, t[1:])
 
 if __name__ == "__main__":
     print(Solution().isIsomorphic(s,t))
